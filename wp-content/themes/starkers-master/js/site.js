b077b0bd377d7929054fd8014b4c001a416f8c69
Ryan Karpeles <rkarpel@jmls.edu>
2013-10-11 12:16:59 -0500

    Add slideout tab css, html, js

--- wp-content/themes/starkers-master/js/site.js
+++ wp-content/themes/starkers-master/js/site.js
@@ -22,7 +22,28 @@
 		// Fitvids.js	
 		
 			// Target your .container, .wrapper, .post, etc.
-			jQuery(".video").fitVids();		
+			jQuery(".video").fitVids();	
+
+		// Slideout Tab
+		    var toggle = false;
+		    $('.slideout-tab').click(function () {
+		   	if (toggle == false) {
+		        $('.slideout').stop().animate({
+		            /* Positive value of 'right' property on .slideout-content in _slideout.scss */
+		            'right': '450px'
+		        });
+		       	toggle = true;
+		    } else {
+		        $('.slideout').stop().animate({
+		            /* Set position back to flush right against edge of browser */
+		            'right': '0px'
+		        });
+		        toggle = false;
+		    }
+});
+
+
+
 
 	});
 
